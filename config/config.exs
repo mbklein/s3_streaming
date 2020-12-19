75507d6bd35d11ec4794e868d6fe5d3ccd737d8d
Michael B. Klein <mbklein@gmail.com>
2020-12-18 22:34:07 -0600

    Use instance role

--- config/config.exs
+++ config/config.exs
@@ -1,5 +1,9 @@
 import Config
 
 config :ex_aws,
-  access_key_id: [{:system, "AWS_ACCESS_KEY_ID"}, {:awscli, "staging", 30}],
-  secret_access_key: [{:system, "AWS_SECRET_ACCESS_KEY"}, {:awscli, "staging", 30}]
+  access_key_id: [{:system, "AWS_ACCESS_KEY_ID"}, {:awscli, "staging", 30}, :instance_role],
+  secret_access_key: [
+    {:system, "AWS_SECRET_ACCESS_KEY"},
+    {:awscli, "staging", 30},
+    :instance_role
+  ]
